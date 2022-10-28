fc417dbe13e841354a20714ff1051e71e972b608
HiwaAbdolahi <89581205+HiwaAbdolahi@users.noreply.github.com>
2022-10-28 14:45:41 +0200

    kjørte min egen test i main for oppgave 5 og resultatet er riktig. oblig3test også passed for oppgave 5.

--- src/main/java/no/oslomet/cs/algdat/Oblig3/Main.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/Main.java
@@ -1,10 +1,31 @@
 package no.oslomet.cs.algdat.Oblig3;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
 
+
+
+        //Lag et nytt binærtre
+        SBinTre<Integer> tre =
+                new SBinTre<>(Comparator.naturalOrder());
+        int[] a = {10, 14, 6, 8, 1, 12, 7, 3, 11, 9, 13, 5, 2, 4};
+        for (int verdi : a) { tre.leggInn(verdi); }
+//Gjør om treet til et array
+        ArrayList<Integer> data = tre.serialize();
+//Lag nytt tre fra arrayet over
+        SBinTre<Integer> tre2 = SBinTre.deserialize(data, Comparator.naturalOrder());
+//Utskriften av tre og tre2 skal være identiske
+        System.out.println(tre.toStringPostOrder());
+        System.out.println(tre2.toStringPostOrder());
+
+
+
+
+
+/*
         Integer[] a = {4,7,2,9,4,10,8,7,4,6};
         SBinTre<Integer> tre = new SBinTre<>(Comparator.naturalOrder());
         for (int verdi : a) { tre.leggInn(verdi); }
@@ -15,6 +36,8 @@ public class Main {
         System.out.println(tre.antall(10)); // Utskrift: 1
 
 
+
+ */
 
 
         /*
